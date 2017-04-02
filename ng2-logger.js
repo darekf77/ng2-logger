7ed2104c5ab66a7a63ea3b11a854fdacbbc3c950
darekf77 <darekf77@gmail.com>
2017-04-02 23:47:22 +0200

    declaration files

--- ng2-logger.js
+++ ng2-logger.js
@@ -2,4 +2,5 @@
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
-__export(require('./js/ng2-logger'));
+__export(require("./src"));
+//# sourceMappingURL=ng2-logger.js.map
--- src/logger.spec.ts
+++ src/logger.spec.ts
@@ -1,34 +1,34 @@
-import {
-    it,
-    inject,
-    injectAsync,
-    beforeEachProviders
-} from '@angular/core/testing';
+// import {
+//     it,
+//     inject,
+//     injectAsync,
+//     beforeEachProviders
+// } from '@angular/core/testing';
 
-// Load the implementations that should be tested
+// // Load the implementations that should be tested
 
-import {provide} from '@angular/core';
-import {Http, HTTP_PROVIDERS, XHRBackend} from '@angular/http';
-import { MockBackend, MockConnection } from '@angular/http/testing';
+// import {provide} from '@angular/core';
+// import {Http, HTTP_PROVIDERS, XHRBackend} from '@angular/http';
+// import { MockBackend, MockConnection } from '@angular/http/testing';
 
-// import { Log } from './logger';
-import { Book } from './test/test-class';
-
-
-// describe('ng2-logger', () => {
-    // provide our implementations or mocks to the dependency injector
-    // beforeEachProviders(() => [
-    //     Http, HTTP_PROVIDERS,
-    //     provide(XHRBackend, { useClass: MockBackend }),
-    //     Log
-    // ]);
+// // import { Log } from './logger';
+// import { Book } from './test/test-class';
 
 
-    // it('should log something', inject([Log], (log: Log<{}>) => {
-    //     Log.er('asdasd');
-    // }));
+// // describe('ng2-logger', () => {
+//     // provide our implementations or mocks to the dependency injector
+//     // beforeEachProviders(() => [
+//     //     Http, HTTP_PROVIDERS,
+//     //     provide(XHRBackend, { useClass: MockBackend }),
+//     //     Log
+//     // ]);
+
+
+//     // it('should log something', inject([Log], (log: Log<{}>) => {
+//     //     Log.er('asdasd');
+//     // }));
 
 
 
 
-// });
+// // });
