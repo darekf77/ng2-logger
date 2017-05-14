de88647ca224f80d33ddace237397db09e9fc085
darekf77 <darekf77@gmail.com>
2017-05-14 20:42:55 +0200

    fix when logging trycahtch any error

--- src/display.ts
+++ src/display.ts
@@ -5,7 +5,7 @@ export class Display {
 
 
     static msg(
-        message: string,
+        message: string | any,
         params: any[],
         moduleName: string,
         moduleColor: string,
@@ -13,26 +13,36 @@ export class Display {
         moduleWidth: number | undefined
     ) {
         if (typeof window != 'undefined' && window.document) {
-        let color = 'gray';
-        if (level === Level.INFO) color = 'deepskyblue';
-        if (level === Level.ERROR) color = 'red';
-        if (level === Level.WARN) color = 'orange';
+            let color = 'gray';
+            if (level === Level.INFO) color = 'deepskyblue';
+            if (level === Level.ERROR) color = 'red';
+            if (level === Level.WARN) color = 'orange';
 
-        if (moduleWidth) {
-            const diff = moduleWidth - moduleName.length;
-            if (diff > 0) {
-                for (let i = 0; i < diff; i++) {
-                    moduleName += ' ';
+            if (moduleWidth) {
+                const diff = moduleWidth - moduleName.length;
+                if (diff > 0) {
+                    for (let i = 0; i < diff; i++) {
+                        moduleName += ' ';
+                    }
                 }
             }
-        }
 
-        let a1 = '%c ' + moduleName + '  %c ' + message + ' ';
-        let a2 = 'background: ' + moduleColor + ';color:white; border: 1px solid ' + moduleColor + '; ';
-        let a3 = 'border: 1px solid ' + color + '; ';
-        params.unshift(a3);
-        params.unshift(a2);
-        params.unshift(a1);
+            if (typeof message === 'string') {
+                let a1 = '%c ' + moduleName + '  %c ' + message + ' ';
+                let a2 = 'background: ' + moduleColor + ';color:white; border: 1px solid ' + moduleColor + '; ';
+                let a3 = 'border: 1px solid ' + color + '; ';
+                params.unshift(a3);
+                params.unshift(a2);
+                params.unshift(a1);
+            } else {
+                let a1 = '%c ' + moduleName + ' ';
+                let a2 = 'background: ' + moduleColor + ';color:white; border: 1px solid ' + color + '; ';
+                params.push(message);
+                params.unshift(a2);
+                params.unshift(a1);
+            }
+
+
         } else {
             params.unshift('[' + moduleName + '] ' + message);
         }
