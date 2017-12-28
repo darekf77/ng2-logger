e2435a45a14e453bc824568a03de3457353652be
Dariusz Filipiak <darekf77@gmail.com>
2017-12-28 22:08:43 +0100

    aaa

--- webpack-bundle.config.js
+++ webpack-bundle.config.js
@@ -1,50 +1,22 @@
 var webpack = require('webpack'),
     path = require('path'),
-    fs = require('fs'),
-    WebpackStrip = require('strip-loader');
-
-
-// var WebpackOnBuildPlugin = require('on-build-webpack');
-
-// var nodeModules = {};
-// fs.readdirSync('node_modules')
-//     .filter(function (x) {
-//         return ['.bin'].indexOf(x) === -1;
-//     })
-//     .forEach(function (mod) {
-//         nodeModules[mod] = 'commonjs ' + mod;
-//     });
+    fs = require('fs');
 
 module.exports = {
-    entry: './index.ts',
+    entry: './index.js',
     output: {
-        // path: __dirname + '/bin',
         libraryTarget: "umd",
         filename: 'bundle.umd.js'
     },
     resolve: {
-        extensions: ['.ts', '.js']
+        extensions: ['.js']
     },
-    module: {
-        loaders: [
-            { test: /\.ts$/, loaders: ['ts-loader'] }
-            // { test: /\.json$/, loaders: ['json-loader'] }
-        ]
-    },
-    // node: {
-    //     fs: "empty",
-    //     __dirname: false,
-    //     __filename: false
-    // },
     plugins: [
         new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false
             }
         })
-        // new WebpackOnBuildPlugin(function (stats) {
-        //     // Do whatever you want... 
-        // }),
     ]
 }
 
