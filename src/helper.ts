6128e7b312072eae8d7d4b16e6806a08564d3205
Dariusz <dariusz.filipiak@igt.com>
2019-03-30 01:16:54 +0100

    simulate browser

--- src/helper.ts
+++ src/helper.ts
@@ -1,5 +1,7 @@
 
 export class Helpers {
+
+  static simulateBrowser = false;
   static isBrowser = !!(typeof window !== 'undefined' && window.document);
   static isNode = (function () {
     Helpers.isNode = !Helpers.isBrowser;
