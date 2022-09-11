2d6e073cd00a660c117b7c7b221991dd578f91be
Dariusz Filipiak <dariusz.filipiak@igt.com>
2022-09-11 23:14:09 +0200

    update

--- src/app/ng2-logger.module.ts
+++ src/app/ng2-logger.module.ts
@@ -2,12 +2,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Ng2LoggerComponent } from './ng2-logger.component';
-import { CanvasDrawerModule } from '@codete-ngrx-quick-start/shared'
-console.log(CanvasDrawerModule)
+// import { CanvasDrawerModule } from '@codete-ngrx-quick-start/shared'
+// console.log(CanvasDrawerModule)
 
 @NgModule({
   imports: [
-    CanvasDrawerModule,
+    // CanvasDrawerModule,
     BrowserModule,
   ],
   declarations: [
