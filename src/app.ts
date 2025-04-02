6bcfeaa2c81d125eb1999417414ef95c727a8e35
Dariusz Filipiak <darekf77@gmail.com>
2025-04-02 20:56:28 +0200

    $$$ update $$$

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,32 @@
-// @ts-nocheck
-import { Taon } from 'taon/src';
-import { Observable, map } from 'rxjs';
-//#region @notForNpm
-import { HOST_BACKEND_PORT } from './app.hosts';
-//#region @browser
-import { NgModule } from '@angular/core';
-import { Component, OnInit } from '@angular/core';
+//#region imports
 import { CommonModule } from '@angular/common';
+import { NgModule, inject, Injectable } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { VERSION } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Taon, BaseContext } from 'taon';
+import { Helpers } from 'tnp-core';
 
+import {
+  HOST_BACKEND_PORT,
+  CLIENT_DEV_WEBSQL_APP_PORT,
+  CLIENT_DEV_NORMAL_APP_PORT,
+} from './app.hosts';
+//#endregion
+
+console.log('hello world');
+console.log('Your server will start on port '+ HOST_BACKEND_PORT);
+const host = 'http://localhost:' + HOST_BACKEND_PORT;
+const frontendHost =
+  'http://localhost:' +
+  (Helpers.isWebSQL ? CLIENT_DEV_WEBSQL_APP_PORT : CLIENT_DEV_NORMAL_APP_PORT);
+
+//#region ng2-logger component
+//#region @browser
 @Component({
   selector: 'app-ng2-logger',
   template: `hello from ng2-logger<br>
+    Angular version: {{ angularVersion }}<br>
     <br>
     users from backend
     <ul>
@@ -19,77 +35,96 @@ import { CommonModule } from '@angular/common';
   `,
   styles: [` body { margin: 0px !important; } `],
 })
-export class Ng2LoggerComponent implements OnInit {
-  users$: Observable<User[]> = User.ctrl.getAll().received.observable
-    .pipe(map(data => data.body.json));
-
-  constructor() { }
-  ngOnInit() { }
+export class Ng2LoggerComponent {
+  angularVersion = VERSION.full;
+  userApiService = inject(UserApiService);
+  readonly users$: Observable<User[]> = this.userApiService.getAll();
 }
+//#endregion
+//#endregion
 
+//#region  ng2-logger api service
+//#region @browser
+@Injectable({
+  providedIn:'root'
+})
+export class UserApiService {
+  userController = Taon.inject(()=> MainContext.getClass(UserController))
+  getAll() {
+    return this.userController.getAll()
+      .received
+      .observable
+      .pipe(map(r => r.body.json));
+  }
+}
+//#endregion
+//#endregion
+
+//#region  ng2-logger module
+//#region @browser
 @NgModule({
-  imports: [CommonModule],
   exports: [Ng2LoggerComponent],
+  imports: [CommonModule],
   declarations: [Ng2LoggerComponent],
-  providers: [],
 })
 export class Ng2LoggerModule { }
 //#endregion
+//#endregion
 
+//#region  ng2-logger entity
 @Taon.Entity({ className: 'User' })
-class User extends Taon.Base.Entity {
-  public static ctrl?: UserController;
+class User extends Taon.Base.AbstractEntity {
   //#region @websql
-  @Taon.Orm.Column.Generated()
+  @Taon.Orm.Column.String()
   //#endregion
-  id?: string | number;
-
+  name?: string;
 }
+//#endregion
 
-@Taon.Controller({ className: 'UserController', entity: User })
-class UserController extends Taon.Base.Controller<User> {
-
+//#region  ng2-logger controller
+@Taon.Controller({ className: 'UserController' })
+class UserController extends Taon.Base.CrudController<User> {
+  entityClassResolveFn = ()=> User;
   //#region @websql
   async initExampleDbData(): Promise<void> {
-    await this.repository.save(new User())
+    const superAdmin = new User();
+    superAdmin.name = 'super-admin';
+    await this.db.save(superAdmin);
   }
   //#endregion
 }
+//#endregion
+
+//#region  ng2-logger context
+var MainContext = Taon.createContext(()=>({
+  host,
+  frontendHost,
+  contextName: 'MainContext',
+  contexts:{ BaseContext },
+  controllers: {
+    UserController,
+    // PUT TAON CONTROLLERS HERE
+  },
+  entities: {
+    User,
+    // PUT TAON ENTITIES HERE
+  },
+  database: true,
+  // disabledRealtime: true,
+}));
+//#endregion
 
 async function start() {
-  console.log('hello world');
-  console.log('Your server will start on port '+ HOST_BACKEND_PORT);
-  const host = 'http://localhost:' + HOST_BACKEND_PORT;
 
-  const context = await Taon.init({
-    host,
-    controllers: [
-      UserController,
-      // PUT FIREDEV CONTORLLERS HERE
-    ],
-    entities: [
-      User,
-      // PUT FIREDEV ENTITIES HERE
-    ],
-    //#region @websql
-    config: {
-      type: 'better-sqlite3',
-      database: 'tmp-db.sqlite',
-      logging: false,
-    }
-    //#endregion
-  });
+  await MainContext.initialize();
 
   if (Taon.isBrowser) {
-    const users = (await User.ctrl.getAll().received).body.json;
+    const users = (await MainContext.getClassInstance(UserController).getAll().received)
+      .body?.json;
     console.log({
-      'users from backend': users
-    })
+      'users from backend': users,
+    });
   }
 }
 
 export default start;
-
-
-
-//#endregion
