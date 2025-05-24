86fef46796d508aa6b98f903643738455f08fb44
Dariusz Filipiak <darekf77@gmail.com>
2025-05-24 19:23:52 +0200

    release: v19.0.20

--- src/lib/build-info._auto-generated_.ts
+++ src/lib/build-info._auto-generated_.ts
@@ -1,6 +1,6 @@
 // THIS FILE IS GENERATED - DO NOT MODIFY
 export const BUILD_FRAMEWORK_CLI_NAME = 'tnp';
 export const PROJECT_NPM_NAME = 'ng2-logger';
-export const CURRENT_PACKAGE_VERSION = '19.0.19';
+export const CURRENT_PACKAGE_VERSION = '19.0.20';
 // THIS FILE IS GENERATED - DO NOT MODIFY
       
