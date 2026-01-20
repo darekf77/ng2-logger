7c50c31afd622be4e3dc6d73cbc98eef23c7f41e
Dariusz Filipiak <darekf77@gmail.com>
2026-01-20 16:28:24 +0100

    chore: api update

--- src/app.electron.ts
+++ src/app.electron.ts
@@ -1,97 +1,97 @@
 
-import { CLIENT_DEV_NORMAL_APP_PORT, CLIENT_DEV_WEBSQL_APP_PORT } from './app.hosts';
-import {
-path,
-//#region @backend
-fse
-//#endregion
-} from 'tnp-core/src';
-//#region @backend
-import { app, BrowserWindow, screen } from 'electron';
+// import { CLIENT_DEV_NORMAL_APP_PORT, CLIENT_DEV_WEBSQL_APP_PORT } from './app.hosts';
+// import {
+// path,
+// //#region @backend
+// fse
+// //#endregion
+// } from 'tnp-core/src';
+// //#region @backend
+// import { app, BrowserWindow, screen } from 'electron';
 
-let win: BrowserWindow | null = null;
-const args = process.argv.slice(1);
-const serve = args.some(val => val === '--serve');
-const websql = args.some(val => val === '--websql');
+// let win: BrowserWindow | null = null;
+// const args = process.argv.slice(1);
+// const serve = args.some(val => val === '--serve');
+// const websql = args.some(val => val === '--websql');
 
-function createWindow(): BrowserWindow {
+// function createWindow(): BrowserWindow {
 
-const size = screen.getPrimaryDisplay().workAreaSize;
+// const size = screen.getPrimaryDisplay().workAreaSize;
 
-// Create the browser window.
-win = new BrowserWindow({
-  x: 0,
-  y: 0,
-  autoHideMenuBar: true,
-  width: size.width / 2,
-  height: size.height / 2,
-  webPreferences: {
-    nodeIntegration: true,
-    allowRunningInsecureContent: (serve),
-    contextIsolation: false,
-  },
-});
+// // Create the browser window.
+// win = new BrowserWindow({
+//   x: 0,
+//   y: 0,
+//   autoHideMenuBar: true,
+//   width: size.width / 2,
+//   height: size.height / 2,
+//   webPreferences: {
+//     nodeIntegration: true,
+//     allowRunningInsecureContent: (serve),
+//     contextIsolation: false,
+//   },
+// });
 
-if (serve) {
-  const debug = require('electron-debug');
-  debug();
+// if (serve) {
+//   const debug = require('electron-debug');
+//   debug();
 
-  require('electron-reloader')(module);
-  win.loadURL('http://localhost:' + (websql ? CLIENT_DEV_WEBSQL_APP_PORT : CLIENT_DEV_NORMAL_APP_PORT));
-} else {
-  // Path when running electron executable
-  let pathIndex = './index.html';
+//   require('electron-reloader')(module);
+//   win.loadURL('http://localhost:' + (websql ? CLIENT_DEV_WEBSQL_APP_PORT : CLIENT_DEV_NORMAL_APP_PORT));
+// } else {
+//   // Path when running electron executable
+//   let pathIndex = './index.html';
 
-  if (fse.existsSync(path.join(__dirname, '../dist/index.html'))) {
-    // Path when running electron in local folder
-    pathIndex = '../dist/index.html';
-  }
+//   if (fse.existsSync(path.join(__dirname, '../dist/index.html'))) {
+//     // Path when running electron in local folder
+//     pathIndex = '../dist/index.html';
+//   }
 
-  const url = new URL(path.join('file:', __dirname, pathIndex));
-  win.loadURL(url.href);
-}
+//   const url = new URL(path.join('file:', __dirname, pathIndex));
+//   win.loadURL(url.href);
+// }
 
-// Emitted when the window is closed.
-win.on('closed', () => {
-  // Dereference the window object, usually you would store window
-  // in an array if your app supports multi windows, this is the time
-  // when you should delete the corresponding element.
-  win = null;
-});
+// // Emitted when the window is closed.
+// win.on('closed', () => {
+//   // Dereference the window object, usually you would store window
+//   // in an array if your app supports multi windows, this is the time
+//   // when you should delete the corresponding element.
+//   win = null;
+// });
 
-return win;
-}
+// return win;
+// }
 
-async function startElectron() {
-try {
-  // This method will be called when Electron has finished
-  // initialization and is ready to create browser windows.
-  // Some APIs can only be used after this event occurs.
-  // Added 400 ms to fix the black background issue while using transparent window. More detais at https://github.com/electron/electron/issues/15947
-  app.on('ready', () => setTimeout(createWindow, 400));
+// async function startElectron() {
+// try {
+//   // This method will be called when Electron has finished
+//   // initialization and is ready to create browser windows.
+//   // Some APIs can only be used after this event occurs.
+//   // Added 400 ms to fix the black background issue while using transparent window. More detais at https://github.com/electron/electron/issues/15947
+//   app.on('ready', () => setTimeout(createWindow, 400));
 
-  // Quit when all windows are closed.
-  app.on('window-all-closed', () => {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-      app.quit();
-    }
-  });
+//   // Quit when all windows are closed.
+//   app.on('window-all-closed', () => {
+//     // On OS X it is common for applications and their menu bar
+//     // to stay active until the user quits explicitly with Cmd + Q
+//     if (process.platform !== 'darwin') {
+//       app.quit();
+//     }
+//   });
 
-  app.on('activate', () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-      createWindow();
-    }
-  });
+//   app.on('activate', () => {
+//     // On OS X it's common to re-create a window in the app when the
+//     // dock icon is clicked and there are no other windows open.
+//     if (win === null) {
+//       createWindow();
+//     }
+//   });
 
-} catch (e) {
-  // Catch Error
-  // throw e;
-}
-}
+// } catch (e) {
+//   // Catch Error
+//   // throw e;
+// }
+// }
 
-export default startElectron;
-//#endregion  
+// export default startElectron;
+// //#endregion
--- src/app.ts
+++ src/app.ts
@@ -1,135 +1,135 @@
-// @ts-nocheck
-//#region imports
-import { CommonModule } from '@angular/common';
-import { NgModule, inject, Injectable } from '@angular/core';
-import { Component, OnInit } from '@angular/core';
-import { VERSION } from '@angular/core';
-import { Observable, map } from 'rxjs';
-import { Taon, TaonBaseContext } from 'taon/src';
-import { Helpers } from 'tnp-core/src';
+// // @ts-nocheck
+// //#region imports
+// import { CommonModule } from '@angular/common';
+// import { NgModule, inject, Injectable } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
+// import { VERSION } from '@angular/core';
+// import { Observable, map } from 'rxjs';
+// import { Taon, TaonBaseContext } from 'taon/src';
+// import { Helpers } from 'tnp-core/src';
 
-import {
-  HOST_BACKEND_PORT,
-  CLIENT_DEV_WEBSQL_APP_PORT,
-  CLIENT_DEV_NORMAL_APP_PORT,
-} from './app.hosts';
-//#endregion
+// import {
+//   HOST_BACKEND_PORT,
+//   CLIENT_DEV_WEBSQL_APP_PORT,
+//   CLIENT_DEV_NORMAL_APP_PORT,
+// } from './app.hosts';
+// //#endregion
 
-console.log('hello world');
-console.log('Your server will start on port ' + HOST_BACKEND_PORT);
-const host = 'http://localhost:' + HOST_BACKEND_PORT;
-const frontendHost =
-  'http://localhost:' +
-  (Helpers.isWebSQL ? CLIENT_DEV_WEBSQL_APP_PORT : CLIENT_DEV_NORMAL_APP_PORT);
+// console.log('hello world');
+// console.log('Your server will start on port ' + HOST_BACKEND_PORT);
+// const host = 'http://localhost:' + HOST_BACKEND_PORT;
+// const frontendHost =
+//   'http://localhost:' +
+//   (Helpers.isWebSQL ? CLIENT_DEV_WEBSQL_APP_PORT : CLIENT_DEV_NORMAL_APP_PORT);
 
-//#region ng2-logger component
-//#region @browser
-@Component({
-  selector: 'app-ng2-logger',
-  template: `hello from ng2-logger<br />
-    Angular version: {{ angularVersion }}<br />
-    <br />
-    users from backend
-    <ul>
-      <li *ngFor="let user of users$ | async">{{ user | json }}</li>
-    </ul> `,
-  styles: [
-    `
-      body {
-        margin: 0px !important;
-      }
-    `,
-  ],
-})
-export class Ng2LoggerComponent {
-  angularVersion = VERSION.full;
-  userApiService = inject(UserApiService);
-  readonly users$: Observable<User[]> = this.userApiService.getAll();
-}
-//#endregion
-//#endregion
+// //#region ng2-logger component
+// //#region @browser
+// @Component({
+//   selector: 'app-ng2-logger',
+//   template: `hello from ng2-logger<br />
+//     Angular version: {{ angularVersion }}<br />
+//     <br />
+//     users from backend
+//     <ul>
+//       <li *ngFor="let user of users$ | async">{{ user | json }}</li>
+//     </ul> `,
+//   styles: [
+//     `
+//       body {
+//         margin: 0px !important;
+//       }
+//     `,
+//   ],
+// })
+// export class Ng2LoggerComponent {
+//   angularVersion = VERSION.full;
+//   userApiService = inject(UserApiService);
+//   readonly users$: Observable<User[]> = this.userApiService.getAll();
+// }
+// //#endregion
+// //#endregion
 
-//#region  ng2-logger api service
-//#region @browser
-@Injectable({
-  providedIn: 'root',
-})
-export class UserApiService {
-  userController = Taon.inject(() => MainContext.getClass(UserController));
-  getAll() {
-    return this.userController
-      .getAll()
-      .received.observable.pipe(map(r => r.body.json));
-  }
-}
-//#endregion
-//#endregion
+// //#region  ng2-logger api service
+// //#region @browser
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class UserApiService {
+//   userController = Taon.inject(() => MainContext.getClass(UserController));
+//   getAll() {
+//     return this.userController
+//       .getAll()
+//       .received.observable.pipe(map(r => r.body.json));
+//   }
+// }
+// //#endregion
+// //#endregion
 
-//#region  ng2-logger module
-//#region @browser
-@NgModule({
-  exports: [Ng2LoggerComponent],
-  imports: [CommonModule],
-  declarations: [Ng2LoggerComponent],
-})
-export class Ng2LoggerModule {}
-//#endregion
-//#endregion
+// //#region  ng2-logger module
+// //#region @browser
+// @NgModule({
+//   exports: [Ng2LoggerComponent],
+//   imports: [CommonModule],
+//   declarations: [Ng2LoggerComponent],
+// })
+// export class Ng2LoggerModule {}
+// //#endregion
+// //#endregion
 
-//#region  ng2-logger entity
-@Taon.Entity({ className: 'User' })
-class User extends Taon.Base.AbstractEntity {
-  //#region @websql
-  @Taon.Orm.Column.String()
-  //#endregion
-  name?: string;
-}
-//#endregion
+// //#region  ng2-logger entity
+// @Taon.Entity({ className: 'User' })
+// class User extends Taon.Base.AbstractEntity {
+//   //#region @websql
+//   @Taon.Orm.Column.String()
+//   //#endregion
+//   name?: string;
+// }
+// //#endregion
 
-//#region  ng2-logger controller
-@Taon.Controller({ className: 'UserController' })
-class UserController extends Taon.Base.CrudController<User> {
-  entityClassResolveFn = () => User;
-  //#region @websql
-  async initExampleDbData(): Promise<void> {
-    const superAdmin = new User();
-    superAdmin.name = 'super-admin';
-    await this.db.save(superAdmin);
-  }
-  //#endregion
-}
-//#endregion
+// //#region  ng2-logger controller
+// @Taon.Controller({ className: 'UserController' })
+// class UserController extends Taon.Base.CrudController<User> {
+//   entityClassResolveFn = () => User;
+//   //#region @websql
+//   async initExampleDbData(): Promise<void> {
+//     const superAdmin = new User();
+//     superAdmin.name = 'super-admin';
+//     await this.db.save(superAdmin);
+//   }
+//   //#endregion
+// }
+// //#endregion
 
-//#region  ng2-logger context
-var MainContext = Taon.createContext(() => ({
-  host,
-  frontendHost,
-  contextName: 'MainContext',
-  contexts: { TaonBaseContext },
-  controllers: {
-    UserController,
-    // PUT TAON CONTROLLERS HERE
-  },
-  entities: {
-    User,
-    // PUT TAON ENTITIES HERE
-  },
-  database: true,
-  // disabledRealtime: true,
-}));
-//#endregion
+// //#region  ng2-logger context
+// var MainContext = Taon.createContext(() => ({
+//   host,
+//   frontendHost,
+//   contextName: 'MainContext',
+//   contexts: { TaonBaseContext },
+//   controllers: {
+//     UserController,
+//     // PUT TAON CONTROLLERS HERE
+//   },
+//   entities: {
+//     User,
+//     // PUT TAON ENTITIES HERE
+//   },
+//   database: true,
+//   // disabledRealtime: true,
+// }));
+// //#endregion
 
-async function start() {
-  await MainContext.initialize();
+// async function start() {
+//   await MainContext.initialize();
 
-  if (Taon.isBrowser) {
-    const users = (
-      await MainContext.getClassInstance(UserController).getAll().received
-    ).body?.json;
-    console.log({
-      'users from backend': users,
-    });
-  }
-}
+//   if (Taon.isBrowser) {
+//     const users = (
+//       await MainContext.getClassInstance(UserController).getAll().received
+//     ).body?.json;
+//     console.log({
+//       'users from backend': users,
+//     });
+//   }
+// }
 
-export default start;
+// export default start;
