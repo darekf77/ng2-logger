fa88fb29c6fbdddc2178cc40535884d343c1cc64
Dariusz Filipiak <darekf77@gmail.com>
2026-01-25 01:12:18 +0100

    chore: update

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,6 +3,8 @@ const angularEslintRecommended = require('@angular-eslint/eslint-plugin').config
 const angularTemplateProcessInlineTemplates = require('@angular-eslint/eslint-plugin-template').configs['process-inline-templates'];
 const prettierConfig = require('eslint-config-prettier');
 const tsRecommended = require("@typescript-eslint/eslint-plugin").configs.recommended;
+const noNamespaceReexport =
+  require('./eslint-rules/no-namespace-reexport').default;
 
 // eslint.config.js
 module.exports = [
@@ -35,6 +37,12 @@ module.exports = [
       "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
       "import": require("eslint-plugin-import"),
       "@angular-eslint": require("@angular-eslint/eslint-plugin"),
+      my: {
+        rules: {
+          'no-namespace-reexport': noNamespaceReexport,
+        },
+      },
+
     },
     rules: {
       // @UNCOMMENT LINE BELOW TO ENABLE TYPESCRIPT RECOMMENDED RULES (STRICT)
@@ -42,6 +50,7 @@ module.exports = [
       ...angularEslintRecommended.rules,
       ...angularTemplateProcessInlineTemplates.rules,
       ...prettierConfig.rules,
+      'my/no-namespace-reexport': 'error',
       '@angular-eslint/prefer-standalone': 'off',
       '@angular-eslint/prefer-inject': 'warn',
       "@angular-eslint/no-empty-lifecycle-method": "off",
--- update-vscode-package-json.js
+++ update-vscode-package-json.js
@@ -4,9 +4,9 @@ const path = require('path');
 const dateformat = require('dateformat');
 const outFolder = path.join(process.cwd(), 'out');
 const fileName = process.argv[2] || 'extension';
-const { crossPlatformPath } = require('tnp-core/lib');
+const { crossPlatformPath, Helpers } = require('tnp-core/lib');
 const { Project } =  require('tnp/lib');
-const { Helpers } = require('tnp-helpers/lib');
+const { HelpersTaon } = require('tnp-helpers/lib');
 
 console.log('Update package.json vscode plugin metadata...');
 
@@ -33,9 +33,9 @@ function updatePackageJson() {
       throw `Resource ${r} not found at ${pathToFileOrFolder}`;
     }
     if(Helpers.isFolder(pathToFileOrFolder)){
-      Helpers.copy(pathToFileOrFolder, dest);
+      HelpersTaon.copy(pathToFileOrFolder, dest);
     } else {
-      Helpers.copyFile(pathToFileOrFolder, dest);
+      HelpersTaon.copyFile(pathToFileOrFolder, dest);
     }
   } );
   console.log(`Using project: ${proj?.name} at ${proj?.location}`);
